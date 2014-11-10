5b0f3451ed0b758ea659ae01e9a5d1083de7af7c
xudshen <xudshen@hotmail.com>
2014-11-10 13:58:39 +0800

    add more example

--- forest/forest_parser.py
+++ forest/forest_parser.py
@@ -56,5 +56,5 @@ if __name__ == "__main__":
     # r = root.xpath("/html/body/table[1]/thead/tr/th[4]/text()")
     # [model.result() for model in ForestModelFactory.values()]
     # ForestModelFactory.get("top10").result()
-    ForestModelFactory.get("lily_user_no_js").result()
+    ForestModelFactory.get("shanghaizufang").result()
     # log_i(ForestModelFactory.get("book"))
